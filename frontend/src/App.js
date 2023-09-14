226bdaa4551ad6f39e28de964859a686a104ae72
ilimKUSH <ilimKUSH>
2023-09-14 12:13:17 +0600

    added product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import {Outlet} from "react-router-dom";
+import {Container} from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 };
